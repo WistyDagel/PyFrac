9e463ea20bc985ad706c708dbaf8178dc24ac32b
WistyDagel <chrisdazley@gmail.com>
2020-10-15 10:23:56 -0600

    Added the methods and created the init function

---
+++ Fraction.py
@@ -0,0 +1,14 @@
+class Fraction: 
+    def __init__(self, wholeNumber, numerator, denominator):
+        self.wholeNumber = wholeNumber
+        self.numerator = numerator
+        self.denominator = denominator
+
+    def makeImproper(self):
+        x = 0
+
+    def makeProper(self): 
+        x = 0
+
+    def gcf(self, Fraction):
+        x = 0
