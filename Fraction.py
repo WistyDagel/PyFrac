94e47835d00b3a4e7375971554a902437143340f
Jeffrey Rehm <47900191+j-rehm@users.noreply.github.com>
2020-10-15 11:12:18 -0600

    implemented some Fraction functions -- needs to be thoroughly tested

--- Fraction.py
+++ Fraction.py
@@ -1,14 +1,48 @@
-class Fraction: 
+def gcf(x, y):
+    result = 1
+
+    if (y == 0):
+        result = x
+    else:
+        result = gcf(y, x % y)
+    
+    if (result < 0):
+        result *= -1
+
+    return result
+
+class Fraction:
     def __init__(self, wholeNumber, numerator, denominator):
         self.wholeNumber = wholeNumber
         self.numerator = numerator
         self.denominator = denominator
 
     def makeImproper(self):
-        x = 0
+        self.numerator = (self.wholeNumber * self.denominator) + self.numerator
+        self.wholeNumber = 0
 
-    def makeProper(self): 
-        x = 0
+    def makeProper(self):
+        if (self.numerator > self.denominator):
+            self.wholeNumber = self.numerator // self.denominator
+            self.numerator = self.numerator % self.denominator
 
-    def gcf(self, Fraction):
-        x = 0
+    def reduce(self):
+        self.makeImproper()
+        # TODO loop? also, does this work properly?
+        factor = gcf(self.numerator, self.denominator)
+        self.numerator //= factor
+        self.denominator //= factor
+        self.makeProper()
+
+    def toString(self):
+        string = ""
+        if (self.wholeNumber > 0):
+            string = f'{self.wholeNumber} {self.numerator}/{self.denominator}'
+        else:
+            string = f'{self.numerator}/{self.denominator}'
+        return string
+
+f1 = Fraction(0, 26, 12)
+print(f1.toString())
+f1.reduce()
+print(f1.toString())
